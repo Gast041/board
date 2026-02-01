1a0d382859fd8315000fefdec698af61bd6ed1bb
Gast041 <gast04@mail.ru>
2026-02-01 19:34:41 +0300

    Регистрация

--- board/urls.py
+++ board/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.shortcuts import render
-
-def home(request):
-    return render(request, "home.html")
+from .views import home_view, signup_view
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", home_view, name="home"),
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("signup/", signup_view, name="signup"),
 ]
---
+++ board/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+def home_view(request):
+    return render(request, "home.html")
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/signup.html", {"form": form})
