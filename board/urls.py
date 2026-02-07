3793d8d3b5d3d163d205759de415f0ff6ae7abc7
Gast041 <gast04@mail.ru>
2026-02-07 17:41:08 +0300

    Разместить объявление

--- board/urls.py
+++ board/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import home_view, signup_view
+from . import views
 
 urlpatterns = [
     path("", home_view, name="home"),
@@ -9,4 +10,5 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("signup/", signup_view, name="signup"),
+    path("ads/create/", views.create_ad, name="create_ad"),
 ]
--- board/views.py
+++ board/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .models import Ad
+
+@login_required
+def create_ad(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        price = request.POST.get("price")
+
+        Ad.objects.create(
+            title=title,
+            description=description,
+            price=price,
+            author=request.user
+        )
+
+        return redirect("/")  # потом заменишь на страницу объявления
+
+    return render(request, "ads/create_ad.html")
 
 def home_view(request):
     return render(request, "home.html")
