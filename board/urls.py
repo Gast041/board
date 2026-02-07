d13a181cfc157b3a6544bf4fb72d9e0fcefc3d19
Gast041 <gast04@mail.ru>
2026-02-07 20:02:42 +0300

    Ошибка 5

--- board/urls.py
+++ board/urls.py
@@ -1,14 +1,38 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .templates.ads.views import home_view, signup_view
-from .templates.ads import views
+
+# Главные вьюхи сайта (главная страница, регистрация)
+from board.views import home_view, signup_view
+
+# Вьюхи приложения объявлений
+from board.ads.views import create_ad
 
 urlpatterns = [
+
+    # Главная страница сайта
     path("", home_view, name="home"),
+
+    # Админка Django
     path("admin/", admin.site.urls),
-    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    # Вход пользователя
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login"
+    ),
+
+    # Выход пользователя
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout"
+    ),
+
+    # Регистрация пользователя
     path("signup/", signup_view, name="signup"),
-    path("ads/create/", views.create_ad, name="create_ad"),
+
+    # Создание объявления (только для авторизованных)
+    path("ads/create/", create_ad, name="create_ad"),
 ]
